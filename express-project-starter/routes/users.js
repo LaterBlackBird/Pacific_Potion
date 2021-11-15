29d47b9a62d390c9a9e29aa8aaa7fc7b6be38398
Seth <seth.holland@gmail.com>
2021-11-15 17:39:15 -0600

    added npm packages for user auth

--- express-project-starter/routes/users.js
+++ express-project-starter/routes/users.js
@@ -1,5 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const { csrfProtection, asyncHandler } = require('./utils');
+const db = require('../db/models');
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const { loginUser, logoutUser } = require('../auth')
+
+const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
