d144d841aa8bbbe62a059b234b82f900c4e887c6
Chloe-Baylock <chloebox523@gmail.com>
2021-11-16 18:59:12 -0500

    created migrations models and relations for comments

---
+++ express-project-starter/db/migrations/20211116234446-create-comment.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      comment: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: "Users"}
+      },
+      potion_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: "Potions"}
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Comments');
+  }
+};
