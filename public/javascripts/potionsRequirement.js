d4d2501837f474b62a88a70278bf5e09e59d4ab9
Chloe-Baylock <chloebox523@gmail.com>
2021-11-17 17:19:11 -0500

    seeding files

---
+++ public/javascripts/potionsRequirement.js
@@ -0,0 +1,8 @@
+// const db = require('../../db/models');
+
+// const comments = db.Comment.findAll({
+//     where: { potion_id: req.params.id }
+// })
+// console.log(comments);
+
+// module.exports = comments;
--- routes/potions.js
+++ routes/potions.js
@@ -7,11 +7,20 @@ const { loginUser, logoutUser } = require('../auth')
 const { userValidators, loginValidators } = require('./authValidations')
 const router = express.Router();
 
+const { comment, user } = require('../public/javascripts/potionsRequirement');
+
 
 /* GET users listing. */
-router.get('/:id(\\d+)', function(req, res, next) {
-  res.render('potion-detail');
-});
+router.get('/:id(\\d+)', asyncHandler(async(req, res, next) => {
+  const comments = await db.Comment.findAll({
+    where: { potion_id: req.params.id }
+  })
+  
+  console.log('****************');
+  console.log(comments);
+  console.log('****************');
+  res.render('potion-detail', { comments });
+}));
 
 
 module.exports = router;
