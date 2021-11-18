7564f64f750555b29863cad5226cd8eaa1408c51
Dominisam98 <dominiqua6@outlook.com>
2021-11-18 12:40:10 -0500

    fixed routes
    fixed routes for adding and deleting potions

--- routes/potions.js
+++ routes/potions.js
@@ -20,14 +20,14 @@ router.get('/:id(\\d+)', csrfProtection, asyncHandler(async (req, res) => {
 }));
 
 /* GET new-potions */
-router.get('/new-potion', csrfProtection, (req, res) => {
+router.get('/potion/add', csrfProtection, (req, res) => {
   const potionTypes = db.PotionType.findAll()
   return res.render('new-potion', { csrfToken: req.csrfToken(), potionTypes });
 });
 
 /* POST new-potions */
 
-router.post('/new-potion', csrfProtection, asyncHandler(async (req, res) => {
+router.post('/potion/add', csrfProtection, asyncHandler(async (req, res) => {
   const { name, description, type } = req.body
   await Potion.create({
     name,
@@ -36,4 +36,33 @@ router.post('/new-potion', csrfProtection, asyncHandler(async (req, res) => {
   });
   res.redirect('/');
 }));
+
+/*DELETE GET potion*/
+
+router.get('/potion/delete/:id(\\d+)', csrfProtection,
+  asyncHandler(async (req, res) => {
+    const potionId = parseInt(req.params.id, 10);
+    const potion = await db.Potion.findByPk(potionId);
+    res.render('potion-delete', {
+      potion,
+      csrfToken: req.csrfToken(),
+    });
+  }));
+
+// // /*POST DELETE potion*/
+
+router.post('/potion/delete/:id(\\d+)', csrfProtection,
+  asyncHandler(async (req, res) => {
+    const potionId = parseInt(req.params.id, 10);
+    const potion = await db.Potion.findByPk(potionId);
+    await potion.destroy();
+    res.redirect('/potions');
+  }));
+
+
+
+
+
+
+
 module.exports = router;
