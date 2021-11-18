4b0f7b5be6e5dc51e7008a0f33034083bfa9d921
Asia <leasia0817@gmail.com>
2021-11-17 17:58:08 -0800

    added user_id to potion table

--- db/migrations/20211116544226-create-potion.js
+++ db/migrations/20211116544226-create-potion.js
@@ -25,6 +25,14 @@ module.exports = {
           key: 'id'
         },
       },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        }
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
--- db/seeders/20211116165144-userSeed.js
+++ db/seeders/20211116165144-userSeed.js
@@ -2,32 +2,14 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
-
-      return queryInterface.bulkInsert('Users', [
-        { username: 'Demo User', role: 'demo', email: 'demo_ppotions@gmail.com', password: 'demo',  createdAt: new Date(), updatedAt: new Date()},
-        { username: 'Administrator', role: 'Admin', email: 'admin_ppotions@gmail.com', password: 'admin',  createdAt: new Date(), updatedAt: new Date()}
+    return queryInterface.bulkInsert('Users', [
+      { username: 'Demo User', role: 'demo', email: 'demo_ppotions@gmail.com', password: 'demo', createdAt: new Date(), updatedAt: new Date() },
+      { username: 'Administrator', role: 'Admin', email: 'admin_ppotions@gmail.com', password: 'admin', createdAt: new Date(), updatedAt: new Date() }
     ], {});
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
-      return queryInterface.bulkDelete('Users', null, {});
+    return queryInterface.bulkDelete('Users', null, {});
   }
 
 };
