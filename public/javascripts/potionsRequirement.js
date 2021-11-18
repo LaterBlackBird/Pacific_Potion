a428ee27e8b63f28c549122057018631f23bc4de
Asia <leasia0817@gmail.com>
2021-11-18 12:27:29 -0800

    submit button not working

--- public/javascripts/potionsRequirement.js
+++ public/javascripts/potionsRequirement.js
@@ -1,41 +1,43 @@
+const db = require("../../db/models");
+
+const createComment = async (potionId, input) => {
+    console.log('Create comment reached!')
+    const potion = db.Potion.findOne({
+        where: {
+            id: potionId
+        },
+        include: db.User
+    });
+    await Comment.create({
+        comment: input,
+        user_id: potion.User.id,
+        potion_id: potionId
+    });
+}
+
 async function postNewComment(url = '', data) {
-    const id = url.split('/potions/')[1];
-    const user = db.User.find
+    console.log('Post fetch request reached!')
+    const potionId = url.split('/potions/')[1];
     const response = await fetch(url, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
+        body: JSON.stringify(data)
     })
-        .then(data => {
-            await Comment.create({
-                comment,
-                user_id,
-                potion_id: id
-            })
-        });
+        .then(data => createComment(potionId, data));
+
     return response.json();
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-    // const ul = document.getElementById('commentContainer');
-    const addCom = document.getElementById('commentSubmit');
-    // const editCom = document.getElementById('editCom');
-    let newComment = document.getElementById('commentInput');
+    const addCom = document.querySelector('#commentSubmit');
 
-    // addCom.addEventListener('click', (e) => {
-    //     // console.log(locals.user.id());
-    //     // let users = await User.findAll();
-    //     // let user = users[0].username;
-    //     // console.log(user);
-    //     e.preventDefault();
-    //     let theirComment = document.createElement('li');
-    //     theirComment.innerText = theirCommentArea.value;
-    //     ul.appendChild(theirComment);
-    //     theirCommentArea.value = '';
-    //     window.scrollTo(0, 1000);
-    // })
     addCom.addEventListener('click', e => {
-
+        console.log('Event Listener reached!')
+        const newComment = document.getElementById('commentInput');
+        return postNewComment('/potions/:id(\\d+)', newComment.value);
     })
 });
+
+window.onload();
