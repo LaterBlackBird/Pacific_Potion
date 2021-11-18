3011b8986d7078bde33886134e1416e67259a1bc
Seth <seth.holland@gmail.com>
2021-11-18 13:09:02 -0600

    search all and search name work from homepage, need to add partial search

--- routes/search.js
+++ routes/search.js
@@ -6,11 +6,29 @@ const bcrypt = require('bcryptjs');
 const { loginUser, logoutUser } = require('../auth')
 const { userValidators, loginValidators } = require('./authValidations')
 const router = express.Router();
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op;
 
 
-router.get('/', asyncHandler(async (req, res) => {
-    const results = await db.Potion.findAll();
-    res.render('search', { results });
+router.post('/', asyncHandler(async (req, res) => {
+    const { search } = req.body
+    if (!search) {
+        const results = await db.Potion.findAll({
+            include: db.PotionType
+        });
+        res.render('search', { results });
+
+    } else {
+        const results = await db.Potion.findAll({
+            where: {
+                name: {
+                    [Op.iLike]: search
+                }
+            },
+            include: db.PotionType
+        });
+        res.render('search', { results });
+    }
 }));
 
 
