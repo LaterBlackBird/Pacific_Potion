b1900b49b1c16cdc6e4db80e907d5867dc603317
Chloe-Baylock <chloebox523@gmail.com>
2021-11-18 12:30:35 -0500

    for now, comments post but are not saved

--- public/javascripts/potionsRequirement.js
+++ public/javascripts/potionsRequirement.js
@@ -1,6 +1,19 @@
-
 document.addEventListener('DOMContentLoaded', () => {
-    const ul = document.getElementById('addCom');
-
+    const ul = document.getElementById('previousCommentsContainer');
+    const addCom = document.getElementById('addCom');
+    const editCom = document.getElementById('editCom');
+    let theirCommentArea = document.getElementById('potionComment');
     
+    addCom.addEventListener('click', (e) => {
+        // console.log(locals.user.id());
+        // let users = await User.findAll();
+        // let user = users[0].username;
+        // console.log(user);
+        e.preventDefault();
+        let theirComment = document.createElement('li');
+        theirComment.innerText = theirCommentArea.value;
+        ul.appendChild(theirComment);
+        theirCommentArea.value = '';
+        window.scrollTo(0, 1000);
+    })
 })
