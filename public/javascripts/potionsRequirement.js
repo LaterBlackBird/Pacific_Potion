0b52543457f305c3829a3c72e9cdc1f0c285f704
Chloe-Baylock <chloebox523@gmail.com>
2021-11-18 19:50:11 -0500

    finalized comments

--- public/javascripts/potionsRequirement.js
+++ public/javascripts/potionsRequirement.js
@@ -13,19 +13,24 @@ async function postNewComment(url = '', data) {
 window.addEventListener('DOMContentLoaded', event => {
     const addCom = document.querySelector('.commentSubmit');
 
-    addCom.addEventListener('click', e => {
+    addCom.addEventListener('click', postComment);
+
+    async function postComment(e) {
         const potionId = e.target.id;
         const newComment = document.getElementById('commentInput');
-        const response = postNewComment(`/potions/${potionId}`, newComment.value); // is a promise. must make it not a promise
+        const response = await postNewComment(`/potions/${potionId}`, newComment.value); // is a promise. must make it not a promise
         const commentContainer = document.getElementById("commentContainer");
         const newCommentDiv = document.createElement("div");
 
-        newCommentDiv.setAttribute("class", "commentItems");
         const newPTag = document.createElement("p");
-        newPTag.innerText = newComment.value;
+        newPTag.innerText = `${response.users.username}: ${response.comm.comment}`;
+
+        newCommentDiv.setAttribute("class", "commentItems")
         newCommentDiv.appendChild(newPTag);
         commentContainer.appendChild(newCommentDiv);
-        newComment.value = "";
+
+
         window.scrollTo(0, 100000);
-    });
+    }
+
 });
