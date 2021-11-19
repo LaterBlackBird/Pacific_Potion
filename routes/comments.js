6ada2ef9fb6421ae5f1f7a57821347f9e41ae676
Seth <seth.holland@gmail.com>
2021-11-19 17:29:41 -0600

    can edit comments, but still need to update the comment card afterward

--- routes/comments.js
+++ routes/comments.js
@@ -35,7 +35,7 @@ router.get('/:id(\\d+)', asyncHandler(async (req, res, next) => {
     if (commentRecord) {
         const comment = commentRecord.comment;
         res.json({ comment });
-    //handle error if comment is not found in database
+        //handle error if comment is not found in database
     } else next(commentNotFound(commentId));
 }));
 
@@ -54,8 +54,8 @@ router.delete('/:id(\\d+)', asyncHandler(async (req, res, next) => {
     if (commentRecord) {
         //destroy record
         await commentRecord.destroy()
-        res.json({message: "Successful"});
-    //handle error if comment is not found in database
+        res.json({ message: "Successful" });
+        //handle error if comment is not found in database
     } else next(commentNotFound(commentId));
 }));
 
@@ -67,16 +67,15 @@ router.put("/:id(\\d+)", asyncHandler(async (req, res) => {
 
     //find the comment being edited
     const comment = await db.Comment.findByPk(commentId)
-    console.log(req);
 
-    //update the comment in the database
-    // if (comment) {
-    //     await comment.update({ comment: req.body.updateText });
-    //     // res.status(202).json({ comment });
-    // //handle error if comment is not found in database
-    // } else {
-    //     next(commentNotFound(commentId));
-    // }
+    // update the comment in the database
+    if (comment) {
+        await comment.update({ comment: req.body.comment });
+        res.json({ message: "Successful" });
+    //handle error if comment is not found in database
+    } else {
+        next(commentNotFound(commentId));
+    }
 }));
 
 module.exports = router
