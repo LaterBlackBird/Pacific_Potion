e7365ad9b65c98486b258bb53239cde5f3f37f0f
Seth <seth.holland@gmail.com>
2021-11-19 11:18:02 -0600

    type filtering on search results fully functional

--- public/javascripts/potionsTypeFilter.js
+++ public/javascripts/potionsTypeFilter.js
@@ -1,12 +1,24 @@
-const fakeButtons = document.querySelectorAll('li')
+window.addEventListener('DOMContentLoaded', event => {
+    //find all the type buttons
+    const typeFilterButton = document.querySelectorAll('li')
 
-for (let i = 0; i < fakeButtons.length; i++) {
-    const button = fakeButtons[i];
+    //for each button listen for a click on each
+    for (let i = 0; i < typeFilterButton.length; i++) {
+        const button = typeFilterButton[i];
 
-    // console.log(`${button.id}`)
-
-    button.addEventListener('click', e => {
-        const potionCards = document.querySelectorAll(`.${button.id}`)
-        potionCards.classList.add = 'glow';
-    })
-}
+        button.addEventListener('click', e => {
+            //find the all result cards from the search
+            const allPotionCards = document.getElementsByClassName('result-card')
+            //pull each card from the HTML Collection of all cards
+            for (let j = 0; j < allPotionCards.length; j++) {
+                const card = allPotionCards[j];
+                //reset hidden attribute if a type is clicked after one has already been selected
+                card.removeAttribute('hidden');
+                //set cards not matching the requested type of be hidden
+                if (card.id !== button.id) {
+                    card.setAttribute('hidden', 'true');
+                }
+            }
+        })
+    }
+});
--- routes/search.js
+++ routes/search.js
@@ -12,13 +12,15 @@ const Op = Sequelize.Op;
 
 router.post('/', asyncHandler(async (req, res) => {
     let { search } = req.body
+
+    //if no search term is defined, return all potions
     if (!search) {
         const results = await db.Potion.findAll({
             order: [['name']],
             include: db.PotionType,
         });
-        res.render('search', { results });
-
+        res.render('search', { results, search });
+    //else return the potion that was asked for
     } else {
         const results = await db.Potion.findAll({
             where: {
@@ -27,11 +29,13 @@ router.post('/', asyncHandler(async (req, res) => {
             order: [['name']],
             include: db.PotionType
         });
-        res.render('search', { results });
+
+        res.render('search', { results, search });
     }
 }));
 
 router.post('/type', asyncHandler(async (req, res) => {
+    //search for the specific potion type asked for
     let { type } = req.body
     const results = await db.Potion.findAll({
         include: [{
@@ -40,10 +44,8 @@ router.post('/type', asyncHandler(async (req, res) => {
         }]
     });
     console.log(results);
-    res.render('search', { results });
+    res.render('search', { results, search });
 }));
-
-
 
 
 module.exports = router;
