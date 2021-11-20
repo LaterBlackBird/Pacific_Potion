be8b6fc456e7a877ed40fbb0052bce2033a791e3
Chloe-Baylock <chloebox523@gmail.com>
2021-11-19 19:18:14 -0500

    fixed filter text

--- public/javascripts/potionsTypeFilter.js
+++ public/javascripts/potionsTypeFilter.js
@@ -9,14 +9,12 @@ window.addEventListener('DOMContentLoaded', event => {
         button.addEventListener('click', e => {
             //find the all result cards from the search
             const allPotionCards = document.getElementsByClassName('result-card')
-            let x = document.querySelector("#results-container h2");
-            let y = x.innerText.split('filtered by ')[1];
-            let z = x.innerText.split('* ')[0];
-            z = z+'*';
-            y = ` ${e.target.innerText} potions`;
-            console.log('z is', z);
-            console.log('y is', y);
-            x.innerText = z + y;
+            let display = document.querySelector("#results-container h2");
+            let nonFilter = display.innerText.split('filtered by')[0];
+            if (nonFilter !== 'You found an empty potion shelf.') {
+                let filter = ` ${e.target.innerText} potions`;
+                display.innerText = nonFilter + "filtered by" + filter;
+            }
             //pull each card from the HTML Collection of all cards
             for (let j = 0; j < allPotionCards.length; j++) {
                 const card = allPotionCards[j];
